5f8ea639bfcf38aa4037e8c400b8f1ccbd432462
liiiira <benyahia.lyes.00@gmail.com>
2026-08-24 16:37:18 +0100

    extend validate to handle params

--- apps/backend/src/middlewares/validate.ts
+++ apps/backend/src/middlewares/validate.ts
@@ -2,15 +2,27 @@ import {type Request, type Response, type NextFunction} from 'express';
 import { AppError } from '../errors/appError';
 import {type ZodType } from 'zod';
 
-export default function validate(schema: ZodType) {
+export default function validate(schema: ZodType, mode: "body"|"params" = "body") {
   return (req: Request, _res: Response, next: NextFunction) => {
 
-    const result = schema.safeParse(req.body);
+    if(mode === "body"){
+      const result = schema.safeParse(req.body);
     
-    if(!result.success)
-      throw new AppError("Invalid Request Body", 400, result.error.issues)
+      if(!result.success)
+        throw new AppError("Invalid Request Body", 400, result.error.issues)
 
-    req.body = result.data;
-    next();
+      req.body = result.data;
+      next();
+    }
+
+    if (mode === "params"){
+
+      const result = schema.safeParse(req.params);
+      
+      if(!result.success)
+        throw new AppError("Invalid Request Params", 400, result.error.issues)
+
+      next();
+    }
   }
 }

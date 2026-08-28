f7c85820b5469ae9348e3a143706276c1b0c239c
liiiira <benyahia.lyes.00@gmail.com>
2026-08-28 16:05:10 +0100

    Change folder structure of frontend

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,22 @@
 import {Route, Routes} from 'react-router';
-
+import { useState } from 'react';
 import './App.css'
 import Layout from './components/layout/layout';
+import { AuthContext } from './context/authContext';
+import ProfilePage from './pages/profilePage';
 
 function App() {
+  const [user, setUser] = useState<Record<string, string>>({userId: '1'})
   return (
+    <AuthContext value={user}>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<p>home</p>} />
-        <Route path="/profile" element={<p>user</p>} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Route>
     
     </Routes>
+    </AuthContext>
   )
 }
 

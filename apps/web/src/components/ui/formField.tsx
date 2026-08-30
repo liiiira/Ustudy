737396ecd8ea7500d312d91e012978ea3bb1bc56
liiiira <benyahia.lyes.00@gmail.com>
2026-08-30 16:24:39 +0100

    update: minor change to form field

--- apps/web/src/components/ui/formField.tsx
+++ apps/web/src/components/ui/formField.tsx
@@ -13,12 +13,11 @@ export default function FormField({id, name, type,  value, placeholder, handleCh
   return (
       <div className="flex flex-col gap-1">
 
-        <label className="text-gray-600" htmlFor="email">{placeholder} </label>
+        <label className=" text-sm text-gray-700 font-medium " htmlFor="email">{placeholder} </label>
       
-        <input className={`bg-white px-4 py-2 border-2 border-gray-200 rounded-md 
-          ${ inputError.length === 0 && value.length > 0? "valid": "" } 
-          ${value.length > 0  && inputError.length > 0 ? "invalid" : ""}
-          [&.invalid]:border-red-500 [&.invalid]:bg-red-50 [&.valid]:border-green-500 [&.valid]:bg-green-50` }
+        <input className={`bg-white px-4 py-2 border-2 border-gray-300 rounded-md outline-none transition-colors
+          ${ inputError.length === 0 && value.length > 0 ? "focus:ring-2 focus:ring-green-500 border-green-500 bg-green-50" : "" } 
+          ${value.length > 0  && inputError.length > 0 ?  "focus:ring-2 focus:ring-red-500 border-red-500 bg-red-50" : ""}`}
             id={id} type={type} name={name} placeholder={placeholder}  onChange={handleChange} required />
 
         <div className=" min-h-5 text-red-500 text-xs">{value.length > 0 && inputError[0]}</div>

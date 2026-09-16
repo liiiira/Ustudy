7b28fba23e35fd0588c030072ef303fbbe54cdde
liiiira <benyahia.lyes.00@gmail.com>
2026-09-16 16:17:45 +0100

    chore(scripts): add migration script for schema migration

---
+++ migrate.sh
@@ -0,0 +1,8 @@
+set -e
+if ! systemctl is-active --quiet docker; then
+  sudo systemctl start docker;
+fi;
+
+docker compose down
+docker compose -f docker-compose.yml up --wait
+pnpm --dir apps/backend migrate

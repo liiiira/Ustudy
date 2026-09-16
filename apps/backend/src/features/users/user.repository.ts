6b2badff2369bd50879143a6280a9ffa8f79b86b
liiiira <benyahia.lyes.00@gmail.com>
2026-09-16 17:07:51 +0100

    fix(user-repository): add avatar to user repository

--- apps/backend/src/features/users/user.repository.ts
+++ apps/backend/src/features/users/user.repository.ts
@@ -3,16 +3,18 @@ import type {User, UserAuth, CreateUserRepository, UpdateUserRepository} from '.
 
 export async function create(userData: CreateUserRepository) : Promise<User>{
 
-  const {username, hashedPassword, email} = userData;
+  const {username, hashedPassword, email, avatarUrl}= userData;
   
   const result = await pool.query(
-    `INSERT INTO users(username, hashed_password, email) Values($1, $2, $3)
+    `INSERT INTO users(username, hashed_password, email, avatar_url) Values($1, $2, $3, $4)
     RETURNING 
       id,
       username,
       email,
-      created_at AS "createdAt"`,
-    [username, hashedPassword, email],
+      created_at AS "createdAt",
+      avatar_url AS "avatarUrl"
+    `,
+    [username, hashedPassword, email, avatarUrl],
   );
 
   return result.rows[0]; 
@@ -21,7 +23,11 @@ export async function create(userData: CreateUserRepository) : Promise<User>{
 export async function findAll() : Promise<User[]>{
   const result = await pool.query(
     `SELECT 
-    id, username, email, created_at  AS "createdAt"
+      id, 
+      username,
+      email, 
+      created_at  AS "createdAt",
+      avatar_url AS "avatarUrl"
     FROM users`
   )
 
@@ -31,7 +37,13 @@ export async function findAll() : Promise<User[]>{
 export async function findByEmail(email: string ): Promise<UserAuth | null>{
 
   const result = await pool.query(
-    `SELECT  id, email, username, hashed_password AS "hashedPassword", created_at AS "createdAt"
+    `SELECT
+      id, 
+      email, 
+      username, 
+      hashed_password AS "hashedPassword",
+      created_at AS "createdAt",
+      avatar_url AS "avatarUrl"
     FROM users
     WHERE email = $1`,
     [email], 
@@ -44,7 +56,13 @@ export async function findByEmail(email: string ): Promise<UserAuth | null>{
 export async function findByUsername(username: string): Promise<UserAuth | null> {
   
   const result = await pool.query(
-    `SELECT  id, email, username, hashed_password AS "hashedPassword", created_at AS "createdAt"
+    `SELECT  
+      id, 
+      email, 
+      username, 
+      hashed_password AS "hashedPassword", 
+      created_at AS "createdAt",
+      avatar_url AS "avatarUrl"
     FROM users
     WHERE username = $1`,
     [username]
@@ -55,7 +73,13 @@ export async function findByUsername(username: string): Promise<UserAuth | null>
 
 export async function findById(id: string): Promise<UserAuth | null>{
    const result = await pool.query(
-    `SELECT id, email, username, hashed_password AS "hashedPassword", created_at AS "createdAt"
+    `SELECT 
+      id, 
+      email, 
+      username, 
+      hashed_password AS "hashedPassword", 
+      created_at AS "createdAt",
+      avatar_url AS "avatarUrl"
     FROM users
     WHERE id = $1`,
     [id]
@@ -66,7 +90,7 @@ export async function findById(id: string): Promise<UserAuth | null>{
 
 export async function updateById(id: string, userData: UpdateUserRepository): Promise<User>{
 
-  const {username, email, hashedPassword} = userData;
+  const {username, email, hashedPassword, avatarUrl} = userData;
   
   // contains the qeury split into strings
   let updates = []
@@ -87,12 +111,22 @@ export async function updateById(id: string, userData: UpdateUserRepository): Pr
     updates.push(`hashed_password = $${values.length + 1}`);
     values.push(hashedPassword);
   }
+  
+  if (avatarUrl){
+    updates.push(`avatar_url = $${values.length + 1}`);
+    values.push(avatarUrl);
+  }
 
   // forming the query
   const query: string = `UPDATE users
     SET ${updates.join(", ")}
     WHERE id = $${values.length + 1}
-    RETURNING id, email, username, created_at AS "createdAt"
+    RETURNING 
+      id, 
+      email, 
+      username, 
+      created_at AS "createdAt",
+      avatar_url AS "avatarUrl"
   `
   values.push(id);
 

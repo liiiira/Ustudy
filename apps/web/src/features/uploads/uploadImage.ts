2a4e268e9279569c143ba459cb744d7dd1548c42
liiiira <benyahia.lyes.00@gmail.com>
2026-09-18 10:15:54 +0100

    feat(uploads): add uploads feature to frontned
    Implementing presignUpload function which returns upload and public url and uploadImage function which uploads the image using the upload url and returns the public Url

---
+++ apps/web/src/features/uploads/uploadImage.ts
@@ -0,0 +1,32 @@
+import { presignUpload } from "./api/uploads.api";
+import type { ContentType, UploadKind } from "./uploads.types";
+import { MAX_UPLOAD_BYTES, CONTENT_TYPES } from "./uploads.types";
+
+export async function uploadImage(file: File, kind: UploadKind){
+
+  if(!CONTENT_TYPES.includes(file.type))
+    throw new Error("Unsupported File type, use PNG, JPEG or WebP.");
+
+  if(file.size > MAX_UPLOAD_BYTES)
+    throw new Error("File is too large, Maxium size: 5MB.")
+
+  const {uploadUrl, publicUrl} = await presignUpload(kind, file.type as ContentType, file.size);
+
+  console.log("file type: ", file.type)
+  console.log("File size: ", file.size)
+
+  const putResponse = await fetch(uploadUrl, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-type": file.type
+    }
+  });
+
+  if(!putResponse.ok)
+    throw new Error("Failed to uplaod image");
+
+  console.log("image uploaded")
+
+  return publicUrl;
+}
---
+++ apps/web/src/features/uploads/uploads.types.ts
@@ -0,0 +1,5 @@
+export type UploadKind = "post" | "avatar" | "community";
+export type ContentType = "image/jpeg" | "image/webp" | "image/png";
+
+export const CONTENT_TYPES = ["image/jpeg", "image/webp", "image/png"]
+export const MAX_UPLOAD_BYTES = 5 * 1024 * 1024;

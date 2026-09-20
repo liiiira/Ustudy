5b0a08c862846491036e6b43ca40d7c39d39b39a
liiiira <benyahia.lyes.00@gmail.com>
2026-09-20 18:54:40 +0100

    feat(messages): add create() function in messages repository

---
+++ apps/backend/src/features/messages/message.repository.ts
@@ -0,0 +1,45 @@
+import pool from "../../config/postgres";
+
+type CreateMessageRepository = {
+  conversationId: string;
+  senderId: string;
+  textContent?: string;
+  uploadId?: string;
+}
+
+export type Message = {
+  id: string;
+  senderUsername?: string;
+  senderId?: string;
+  createdAt: Date;
+  imageUrl?: string;
+  textContent?: string;
+  conversationId?: string;
+}
+export async function create({conversationId, senderId, textContent, uploadId}: CreateMessageRepository) {
+
+  const result = await pool.query(`
+    WITH inserted AS(
+      INSERT INTO messages(conversation_id, sender_id, text_content, upload_id)
+      VALUES 
+        ($1, $2, $3, $4)
+      RETURNING *
+    )
+    SELECT 
+      inserted.id AS "id",
+      inserted.sender_id AS "senderId",
+      user.username AS "senderUsername",
+      inserted.text_content AS "textContent",
+      inserted.conversation_id AS "conversationId",
+      inserted.created_at AS "createdAt",
+      uploads.public_url AS "imageUrl"
+    FROM inserted 
+    LEFT JOIN uploads
+      ON inserted.upload_id = uploads.id
+    LEFT JOIN users 
+      ON inserted.send_id = users.id`,
+    [conversationId, senderId, textContent, uploadId]
+  );
+
+  return result.rows[0] ?? null;
+}

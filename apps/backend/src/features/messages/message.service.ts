fb22feb526b50750381e3e35c45d9c09b69b7292
liiiira <benyahia.lyes.00@gmail.com>
2026-09-21 13:39:06 +0100

    feat(conversations): Implemented create() in conversations service that creates a message

---
+++ apps/backend/src/features/messages/message.service.ts
@@ -0,0 +1,28 @@
+import * as messageRepository from "./message.repository.ts";
+import * as uploadService from "../uploads/upload.service.ts";
+import * as conversationService from "../conversations/conversation.service.ts";
+import type { Message, MessageInput } from "./message.schema.ts";
+import { AppError } from "../../errors/appError.ts";
+
+
+export async function create(requesterId: string, {textContent, imageUrl, conversationId}: MessageInput){
+
+  if(!textContent && !imageUrl) throw new AppError("Message must contain at least a text or an image", 400);
+  
+  const conversation = await conversationService.findById(conversationId);
+  if(!conversation) throw new AppError("Conversation Not found", 404);
+
+  const requesterMember = await conversationService.findMember(conversationId, requesterId);
+  if(!requesterMember) throw new AppError("Not a member of this conversation", 403);
+  
+  let uploadId: string | undefined;
+  if(imageUrl){
+    const upload = await uploadService.verifyUploadOwnerShip(requesterId, {conversationUrl: imageUrl});
+    uploadId = upload.id;
+  } 
+
+  const createdMessage: Message | null = await messageRepository.create({conversationId, senderId: requesterId, textContent, uploadId})
+  if(!createdMessage) throw new AppError("Failed to create message due to unnexpected error", 500);
+
+  return createdMessage;
+}

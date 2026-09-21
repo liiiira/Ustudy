ba06ce486e07290adbcaeee83693566120bd9880
liiiira <benyahia.lyes.00@gmail.com>
2026-09-21 13:51:27 +0100

    feat(conversations): add conversation create() to conversations controller

---
+++ apps/backend/src/features/messages/message.controller.ts
@@ -0,0 +1,19 @@
+import type { Message, MessageInput } from "./message.schema.ts";
+import * as messageService from "./message.service.ts";
+import type {Request, Response} from "express";
+
+
+export async function create(req: Request<{conversationId: string}>, res: Response){
+  
+  const userId = req.user!.id;
+  const {conversationId} = req.params;
+  const requestBody: MessageInput  = req.body;
+
+  const createdMessage: Message = await messageService.create(userId, conversationId, requestBody);
+
+  return res.status(201).json({
+    status: "success",
+    message: "Message Created Sucessfully",
+    messageObject: createdMessage,
+  })
+}

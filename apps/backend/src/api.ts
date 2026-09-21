147f9ee1a385da19570cdd3fcd683f79e2b59138
liiiira <benyahia.lyes.00@gmail.com>
2026-09-21 13:58:41 +0100

    feat(conversations): add POST /conversations/:conversationId/messages (create a message)

--- apps/backend/src/api.ts
+++ apps/backend/src/api.ts
@@ -6,6 +6,7 @@ import postRouter from "./features/posts/post.route.ts"
 import commentRouter from "./features/comments/comment.route.ts" 
 import uploadRouter from "./features/uploads/uploads.route.ts"
 import conversationRouter from "./features/conversations/conversation.route.ts"
+import messageRouter from "./features/messages/message.route.ts";
 
 const router = Router();
 
@@ -13,7 +14,8 @@ router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/communities", communityRouter);
 router.use("/uploads", uploadRouter);
-router.use("/conversations", conversationRouter)
+router.use("/conversations", conversationRouter);
+router.use("conversations/:conversationId", messageRouter);
 router.use("/communities/:communityId/posts", postRouter);
 router.use("/communities/:communityId/posts/:postId/comments", commentRouter);
 
---
+++ apps/backend/src/features/messages/message.route.ts
@@ -0,0 +1,19 @@
+import * as messageController from "./message.controller.ts";
+import { Router } from "express";
+import isAuthenticated from "../../middlewares/isAuthenticated";
+import { validateBody, validateParams } from "../../middlewares/validate";
+import { createMessageSchema } from "./message.schema";
+import { conversationIdSchema } from "../conversations/conversation.schema.ts";
+
+const router = Router({caseSensitive: true, mergeParams: true});
+
+router.post("/messages", 
+  isAuthenticated,
+  validateParams(conversationIdSchema),
+  validateBody(createMessageSchema),
+  messageController.create
+)
+
+
+
+export default router;
